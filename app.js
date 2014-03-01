c77e67c3e8339cef631bdd24c56c1fe79c889a29
Alex Bezuska <abezuska@gmail.com>
2014-03-01 16:43:56 -0500

    add function to write out to file

--- app.js
+++ app.js
@@ -12,17 +12,22 @@ var fs = require('fs');
 var merge = require('merge');
 
 var data  = JSON.parse(fs.readFileSync(dataFileName, {encoding : 'utf8'}));
-data.page = merge(data.page, data.pages[templateFileName]);
-
-var layoutFileName = data.page.layout;
-var layout  = fs.readFileSync(layoutFileName, {encoding : 'utf8'});
-var page  = fs.readFileSync(templateFileName, {encoding : 'utf8'});
-console.log(data);
-
-handlebars.registerPartial('body', page);
-var template = handlebars.compile(layout);
-console.log(template(data));
 
 
 
+function build ( sourcePath, destinationPath, context){
 
+  data.page = merge(data.page, data.pages[templateFileName]);
+
+  var layoutFileName = context.page.layout;
+  var layout  = fs.readFileSync(layoutFileName, {encoding : 'utf8'});
+  var page  = fs.readFileSync(templateFileName, {encoding : 'utf8'});
+  console.log(context);
+
+  handlebars.registerPartial('body', page);
+  var template = handlebars.compile(layout);
+  
+  fs.writeFileSync(destinationPath, template(context), {encoding : 'utf8'});
+};
+
+build( templateFileName, "out.html", data);

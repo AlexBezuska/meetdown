63fad6aeb252ef6d9e365b715b912d2c09e15f6b
Alex Bezuska <abezuska@gmail.com>
2014-03-01 16:18:55 -0500

    load layout from JSON

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 var args = process.argv.slice(2);
 
-if( args.length != 3){
-  console.error("Usage: node app.js layout.hbs template.hbs template.json ");
+if( args.length != 2){
+  console.error("Usage: node app.js page.hbs data.json ");
   process.exit(1);
 }
+var templateFileName = args[0];
+var dataFileName = args[1];
 
 var handlebars = require('handlebars');
 var fs = require('fs');
 
-var layout  = fs.readFileSync(args[0], {encoding : 'utf8'});
-var source  = fs.readFileSync(args[1], {encoding : 'utf8'});
-var data  = JSON.parse(fs.readFileSync(args[2], {encoding : 'utf8'}));
-data.page = data.pages[args[1]];
+var data  = JSON.parse(fs.readFileSync(dataFileName, {encoding : 'utf8'}));
+data.page = data.pages[templateFileName];
+
+var layoutFileName = data.page.layout;
+var layout  = fs.readFileSync(layoutFileName, {encoding : 'utf8'});
+var page  = fs.readFileSync(templateFileName, {encoding : 'utf8'});
 console.log(data);
 
-handlebars.registerPartial('body', source);
+handlebars.registerPartial('body', page);
 var template = handlebars.compile(layout);
 console.log(template(data));
 
